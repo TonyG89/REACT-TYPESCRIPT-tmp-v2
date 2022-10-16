aac6fe381a4c54d84f4c8c090ef871a82f4bbd6f
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-10-16 15:40:03 +0300

    fetch-get do it

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './scss/app.scss';
 import Header from './components/Header'
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import ClothesBlock from './components/ClothesBlock';
+import ClothesBlock from './components/ClothesBlock/ClothesBlock';
 
-import clothes from './assets/blank_clothes.json'
+// import clothes from './assets/blank_clothes.json'
 
 
 function App() {
+
+  const [clothes, setClothes] = React.useState([])
+React.useEffect(()=>{
+  fetch("https://630927d6722029d9dddf3c35.mockapi.io/blank_clothes")
+    .then((res) => res.json())
+    .then(arr => setClothes(arr))
+}, [])
+
   return (
     <>
       <div className="App">
@@ -17,7 +25,7 @@ function App() {
           <div className="content">
             <div className="container">
               <div className="content__top">
-                {/* <Categories /> */}
+                <Categories />
                 <Sort />
               </div>
               <h2 className="content__title">Усі</h2>
@@ -25,11 +33,11 @@ function App() {
                 {
                   clothes.map(obj => (
                     <ClothesBlock key={obj.id}
-                    title={obj.title + " " + obj.color} 
-                    price={obj.price} 
-                    link={obj.link} 
-                    brand={obj.brand} 
-                    sizes={obj.size} />
+                      title={obj.title + " " + obj.color}
+                      price={obj.price}
+                      link={obj.link}
+                      brand={obj.brand}
+                      sizes={obj.size} />
                   ))
                 }
               </div>
